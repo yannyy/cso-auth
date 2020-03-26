e039671901e40e799bc82a2d66c0742d2e85acf0
yan yang <yan.yang0829@gmail.com>
2020-03-26 18:49:46 +0800

    [auth]:impl

---
+++ go.mod
@@ -0,0 +1,8 @@
+module cso-api/auth
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/micro/go-micro v1.18.0
+)
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/micro/go-micro"
+
+	auth "cso-api/auth/proto"
+)
+
+type Auth struct {
+}
+
+func (a *Auth) Message(ctx context.Context, req *auth.Empty, rsp *auth.Response) error {
+	return nil
+}
+
+func main() {
+	service := micro.NewService(
+		micro.Name("cso.auth"),
+		micro.RegisterTTL(time.Second*30),
+		micro.RegisterInterval(time.Second*10),
+	)
+
+	service.Init()
+
+	auth.RegisterAuthHandler(service.Server(), new(Auth))
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
